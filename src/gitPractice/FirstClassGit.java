4ab76d98b2748fbab810fe9107e2fab6f21ee1dd
taufiqnazary <117698076+taufiqnazary@users.noreply.github.com>
2023-02-06 21:09:06 -0500

    Update FirstClassGit.java
    Methods Removed

--- src/gitPractice/FirstClassGit.java
+++ src/gitPractice/FirstClassGit.java
@@ -2,16 +2,4 @@ package gitPractice;
 
 public class FirstClassGit {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println ("This is Test from Local Repo");
-
-	}
-	
-	public void test() {
-		// TODO Auto-generated method stub
-		System.out.println ("This is Test from Local Repo 2 to be witnessed");
-
-}
-	
 }
